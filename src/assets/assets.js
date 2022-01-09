969c7f9c8d0364c94cf74e2b49e35f0db9f3fc39
Jongwoo Park <jongwoo.park@code.berlin>
2022-01-09 17:12:27 +0100

    image optimized

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,58 +1,276 @@
+// const assets = {
+//   base: {
+//     1: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618ef1b2cba82105230db79f_211031_03_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     2: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e21662e7fb35e3400e0b1_211031_01_3_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     3: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e20a8fef332e147473f34_211031_01_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     4: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e17f4f9ab53640b154395_211031_03_3_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     5: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e16a0e10e33eb3684b06e_211031_03_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     6: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf4332686182df3b56be_211031_10_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     7: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf08a0adfa7fb47f0311_211031_09_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     8: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdeeff945d4243c55cddf_211031_08_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     9: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdede9e94526b55a2eb2f_211031_07_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     10: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e20a8fef332e147473f34_211031_01_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     11: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fe02b92067f477b8af84e_211031_02_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     12: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e1223e10e33af98849e77_211031_02_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     13: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e0477d1873c2fb06aa95e_211031_01_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5%20(1).png",
+//     },
+//     14: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf354627803d9bc73d6c_211031_09_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//     15: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fded1a0a12825d0bdf0c8_211031_06_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+//     },
+//   },
+//   namso: {
+//     1: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cea66542f27418ca6db_23.png",
+//       alt: "외국인으로 살고 있기 때문에 카카오뱅크 통장을 만들 수 없어요",
+//     },
+//     2: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d7e2453cb30ab13c56f_28.png",
+//       alt: "지리적, 서류상 고향",
+//     },
+//     3: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02c815662f73d46950de0_17.png",
+//       alt: "한국국적을 잃었던 적이 있어요",
+//     },
+//     4: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d6bac5634f84070ee58_27.png",
+//       alt: "임의로 만든 이름",
+//     },
+//     5: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cfe00fc8a22997b9579_24.png",
+//       alt: "코로나가 터진 이후 한국의 의료시스템을 따라올 나라가 없다는 것을 알고 어떤 국적을 가질지 더 고민하게 되었죠",
+//     },
+//     6: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02c57110ab5cd7c90ab1b_16.png",
+//       alt: "엄밀히 따지면 '대만인'과 마찬가지로 '홍콩인'이라는 단어는 존재하지 않는다",
+//     },
+//     7: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d9961c75412c27a6e70_30.png",
+//       alt: "홍콩 대사관이라는 것은 존재하지 않는다",
+//     },
+//     8: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02ca5c26837e98a2095a1_19.png",
+//       alt: "이름의 철자가 실제 이름의 발음과 달라 매번 수정해줘야 하는 번거로움",
+//     },
+//     9: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d8b1c5fbc84e8370f9a_29.png",
+//       alt: "제 이름을 최대한 한국말과 가깝게 읽을 수 있는 스펠링을 고민했어요",
+//     },
+//     10: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cdb0248ef7e6df723e9_22.png",
+//       alt: "세금을 두 곳에 내야 합니다",
+//     },
+//   },
+//   eun: {
+//     1: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02f31aa777b263a2c0bd2_31.png",
+//       alt: "서류상 이중국적자",
+//     },
+//     2: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02f698bfa118174c27fa3_32.png",
+//       alt: "두 국가에 각각  마음적 고향이 있다",
+//     },
+//     3: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02f8f2453cb68d613d1e2_33.png",
+//       alt: "이방인",
+//     },
+//     4: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02fa7baf30ff9e8034f77_34.png",
+//       alt: "고향은 할머니가 계신 곳",
+//     },
+//     5: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02fc7b671135362f70284_35.png",
+//       alt: "고향에 대한 그리움, 노스텔지아 같은 감정은 없어요",
+//     },
+//     6: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02fe1b6a62c2f88865c3b_36.png",
+//       alt: "국적은 제도적인 이득, 권리를 누릴 수 있는 하나의 자격 정도",
+//     },
+//     7: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03040334037a917ab8455_37.png",
+//       alt: "하나의 언어 안에 있는 수 많은 아름다운 요소를 제대로 누리지 못할 때",
+//     },
+//     8: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0305bc268372ba920ae9e_38.png",
+//       alt: "과테말라 영주권이 있었던 적이 있어요",
+//     },
+//     9: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03071121c6ba186a243f1_39.png",
+//       alt: "재외국민",
+//     },
+//     10: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c030b96ee1497d12c79725_40.png",
+//       alt: "가족이 있는 곳의 국적을 갖고 싶어요",
+//     },
+//     11: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c030c91c5fbcd3c637310b_41.png",
+//       alt: "함경도이탈주민 = 대구이탈주민",
+//     },
+//     12: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03102448248587a4f5118_42.png",
+//       alt: "검머외",
+//     },
+//     13: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c031147ea68aa79430e88b_43.png",
+//       alt: "서류의 일부일 뿐인 국적 정체성",
+//     },
+//     14: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0312e6c4806043fc2d88a_44.png",
+//       alt: "어떤 탈북 친구들은 강원도에서 왔다고 하지요",
+//     },
+//     15: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03142b6a62cc8a2866924_45.png",
+//       alt: "존재하지 않는 모국어를 해보라는 요청",
+//     },
+//     16: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03156ac563462bb71059b_46.png",
+//       alt: "문화적 시소",
+//     },
+//     17: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03167110ab595a190d45f_47.png",
+//       alt: "'토종'이라고 불렸었다",
+//     },
+//     18: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0317a8bd674ce70de2c28_48.png",
+//       alt: "이름에서 성이 앞으로  오는지 뒤로 오는지의 차이만 있을 뿐",
+//     },
+//     19: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c031928752dd0f43210cf0_49.png",
+//       alt: "인간 중에 소수가 사람이지요",
+//     },
+//     20: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c031a9110ab579c390d47a_50.png",
+//       alt: "영어 이름의 나를 그냥 '그 시절의 나'로만 기억해요",
+//     },
+//   },
+//   hyunchul: {
+//     1: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0286f110ab526a8908d33_1.png",
+//       alt: "서울 압구정동 윤호병원 1기생",
+//     },
+//     2: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0287f0b94f3cb3817a459_2.png",
+//       alt: "0개국어를 하는 느낌이죠",
+//     },
+//     3: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02890e34f90fea4e88c2a_3.png",
+//       alt: "네덜란드 국적을 갖고 싶습니다",
+//     },
+//     4: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028af49e78a2a884005b5_4.png",
+//       alt: "지리적 고향에 대한 큰 의미를 갖고 있지 않습니다",
+//     },
+//     5: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028c1194ffa045ab61627_5.png",
+//       alt: "구사할 수 있는 언어는 2.5개입니다",
+//     },
+//     6: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028d26ee14981f3c7707d_6.png",
+//       alt: "왜 여기서 머물러야 하는지 구구절절한 설명과 여권, 거주증을 보여줘야 해요",
+//     },
+//     7: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028da110ab553be909204_7.png",
+//       alt: "어디서든 이방인",
+//     },
+//     8: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028edd7afbc38e653ab09_8.png",
+//       alt: "스타벅스에 가면 여러 버전의 이름이 생깁니다",
+//     },
+//     9: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028fe00fc8a8b507b76d2_9.png",
+//       alt: "독일을 선택했다기 보다 집에서 최대한 멀어지고 싶었다",
+//     },
+//     10: {
+//       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02a35b5e590372de5152f_10.png",
+//       alt: "누가 나를 어떻게 부르든 찰떡같이 알아듣게 됐습니다",
+//     },
+//   },
+// };
+
+// export default assets;
+
 const assets = {
   base: {
     1: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618ef1b2cba82105230db79f_211031_03_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618ef1b2cba82105230db79f_211031_03_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     2: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e21662e7fb35e3400e0b1_211031_01_3_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e21662e7fb35e3400e0b1_211031_01_3_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     3: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e20a8fef332e147473f34_211031_01_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e1aff978d65b31665f8a1_211031_01_5_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     4: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e17f4f9ab53640b154395_211031_03_3_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e17f4f9ab53640b154395_211031_03_3_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     5: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e16a0e10e33eb3684b06e_211031_03_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e16a0e10e33eb3684b06e_211031_03_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     6: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf4332686182df3b56be_211031_10_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf4332686182df3b56be_211031_10_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     7: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf08a0adfa7fb47f0311_211031_09_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf08a0adfa7fb47f0311_211031_09_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     8: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdeeff945d4243c55cddf_211031_08_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdeeff945d4243c55cddf_211031_08_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     9: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdede9e94526b55a2eb2f_211031_07_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdede9e94526b55a2eb2f_211031_07_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     10: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e20a8fef332e147473f34_211031_01_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e20a8fef332e147473f34_211031_01_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     11: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fe02b92067f477b8af84e_211031_02_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fe02b92067f477b8af84e_211031_02_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     12: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e1223e10e33af98849e77_211031_02_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e1223e10e33af98849e77_211031_02_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     13: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e0477d1873c2fb06aa95e_211031_01_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5%20(1).png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618e0477d1873c2fb06aa95e_211031_01_1_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5%20(1)-p-500.png",
     },
     14: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf354627803d9bc73d6c_211031_09_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fdf354627803d9bc73d6c_211031_09_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
     15: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fded1a0a12825d0bdf0c8_211031_06_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/618fded1a0a12825d0bdf0c8_211031_06_2_%E1%84%82%E1%85%AE%E1%84%81%E1%85%B5-p-500.png",
     },
   },
   namso: {
     1: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cea66542f27418ca6db_23.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cea66542f27418ca6db_23-p-500.png",
       alt: "외국인으로 살고 있기 때문에 카카오뱅크 통장을 만들 수 없어요",
     },
     2: {
-      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d7e2453cb30ab13c56f_28.png",
+      link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d7e2453cb30ab13c56f_28-p-500.png",
       alt: "지리적, 서류상 고향",
     },
     3: {
