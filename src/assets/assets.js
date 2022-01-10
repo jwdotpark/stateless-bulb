001b727a918ae4f948a529ee42bfdf2ddd350060
Jongwoo Park <jongwoo.park@code.berlin>
2022-01-10 15:08:59 +0100

    asset amended

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -267,109 +267,109 @@ const assets = {
   namso: {
     1: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cea66542f27418ca6db_23-p-500.png",
-      alt: "외국인으로 살고 있기 때문에 카카오뱅크 통장을 만들 수 없어요",
+      alt: "외국인으로 살고 있기 때문에 카카오뱅크 통장을 만들 수 없었어요.",
     },
     2: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d7e2453cb30ab13c56f_28-p-500.png",
-      alt: "지리적, 서류상 고향",
+      alt: "",
     },
     3: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02c815662f73d46950de0_17.png",
-      alt: "한국국적을 잃었던 적이 있어요",
+      alt: "한국국적을 잃었던 적이 있어요.",
     },
     4: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d6bac5634f84070ee58_27.png",
-      alt: "임의로 만든 이름",
+      alt: "",
     },
     5: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cfe00fc8a22997b9579_24.png",
-      alt: "코로나가 터진 이후 한국의 의료시스템을 따라올 나라가 없다는 것을 알고 어떤 국적을 가질지 더 고민하게 되었죠",
+      alt: "코로나가 터진 이후 한국의 의료시스템을 따라올 나라가 없다는 것을 알고 어떤 국적을 가질지 더 고민하게 되었죠.",
     },
     6: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02c57110ab5cd7c90ab1b_16.png",
-      alt: "엄밀히 따지면 '대만인'과 마찬가지로 '홍콩인'이라는 단어는 존재하지 않는다",
+      alt: "엄밀히 따지면 '대만인'과 마찬가지로 '홍콩인'이라는 단어는 존재하지 않아요.",
     },
     7: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d9961c75412c27a6e70_30.png",
-      alt: "홍콩 대사관이라는 것은 존재하지 않는다",
+      alt: "홍콩은 마음속에 확실한 하나의 국가입니다.",
     },
     8: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02ca5c26837e98a2095a1_19.png",
-      alt: "이름의 철자가 실제 이름의 발음과 달라 매번 수정해줘야 하는 번거로움",
+      alt: "이름의 철자가 실제 이름의 발음과 달라 매번 수정해줘야 하는 번거로움이 있어요.",
     },
     9: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02d8b1c5fbc84e8370f9a_29.png",
-      alt: "제 이름을 최대한 한국말과 가깝게 읽을 수 있는 스펠링을 고민했어요",
+      alt: "제 이름을 최대한 한국말과 가깝게 읽을 수 있는 스펠링을 고민했어요.",
     },
     10: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02cdb0248ef7e6df723e9_22.png",
-      alt: "세금을 두 곳에 내야 합니다",
+      alt: "세금을 두 곳에 내야 합니다.",
     },
   },
   eun: {
     1: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02f31aa777b263a2c0bd2_31.png",
-      alt: "서류상 이중국적자",
+      alt: "서류상 이중국적자입니다.",
     },
     2: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02f698bfa118174c27fa3_32.png",
-      alt: "두 국가에 각각  마음적 고향이 있다",
+      alt: "두 국가에 각각  마음적 고향이 있다.",
     },
     3: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02f8f2453cb68d613d1e2_33.png",
-      alt: "이방인",
+      alt: "",
     },
     4: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02fa7baf30ff9e8034f77_34.png",
-      alt: "고향은 할머니가 계신 곳",
+      alt: "고향은 할머니가 계신 곳입니다.",
     },
     5: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02fc7b671135362f70284_35.png",
-      alt: "고향에 대한 그리움, 노스텔지아 같은 감정은 없어요",
+      alt: "고향에 대한 그리움, 노스텔지아 같은 감정은 없어요.",
     },
     6: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02fe1b6a62c2f88865c3b_36.png",
-      alt: "국적은 제도적인 이득, 권리를 누릴 수 있는 하나의 자격 정도",
+      alt: "국적은 제도적인 이득, 권리를 누릴 수 있는 하나의 자격 정도입니다.",
     },
     7: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03040334037a917ab8455_37.png",
-      alt: "하나의 언어 안에 있는 수 많은 아름다운 요소를 제대로 누리지 못할 때",
+      alt: "하나의 언어 안에 있는 수 많은 아름다운 요소를 제대로 누리지 못할 때 아쉬워요.",
     },
     8: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0305bc268372ba920ae9e_38.png",
-      alt: "과테말라 영주권이 있었던 적이 있어요",
+      alt: "과테말라 영주권이 있었던 적이 있어요.",
     },
     9: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03071121c6ba186a243f1_39.png",
-      alt: "재외국민",
+      alt: "제외국민 혹은 재외국민이라고 읽혀요.",
     },
     10: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c030b96ee1497d12c79725_40.png",
-      alt: "가족이 있는 곳의 국적을 갖고 싶어요",
+      alt: "",
     },
     11: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c030c91c5fbcd3c637310b_41.png",
-      alt: "함경도이탈주민 = 대구이탈주민",
+      alt: "함경도이탈주민은 대구이탈주민과 같아요.",
     },
     12: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03102448248587a4f5118_42.png",
-      alt: "검머외",
+      alt: "",
     },
     13: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c031147ea68aa79430e88b_43.png",
-      alt: "서류의 일부일 뿐인 국적 정체성",
+      alt: "서류의 일부일 뿐인 국적 정체성입니다.",
     },
     14: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0312e6c4806043fc2d88a_44.png",
-      alt: "어떤 탈북 친구들은 강원도에서 왔다고 하지요",
+      alt: "",
     },
     15: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03142b6a62cc8a2866924_45.png",
-      alt: "존재하지 않는 모국어를 해보라는 요청",
+      alt: "존재하지 않는 모국어를 해보라는 요청을 받습니다.",
     },
     16: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03156ac563462bb71059b_46.png",
-      alt: "문화적 시소",
+      alt: "문화적 시소를 갖고 있습니다.",
     },
     17: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c03167110ab595a190d45f_47.png",
@@ -377,57 +377,57 @@ const assets = {
     },
     18: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0317a8bd674ce70de2c28_48.png",
-      alt: "이름에서 성이 앞으로  오는지 뒤로 오는지의 차이만 있을 뿐",
+      alt: "이름에서 성이 앞으로  오는지 뒤로 오는지의 차이만 있을 뿐  입니다.",
     },
     19: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c031928752dd0f43210cf0_49.png",
-      alt: "인간 중에 소수가 사람이지요",
+      alt: "",
     },
     20: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c031a9110ab579c390d47a_50.png",
-      alt: "영어 이름의 나를 그냥 '그 시절의 나'로만 기억해요",
+      alt: "영어 이름의 나를 그냥 '그 시절의 나'로만 기억해요.",
     },
   },
   hyunchul: {
     1: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0286f110ab526a8908d33_1-p-500.png",
-      alt: "서울 압구정동 윤호병원 1기생",
+      alt: "",
     },
     2: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c0287f0b94f3cb3817a459_2-p-500.png",
-      alt: "0개국어를 하는 느낌이죠",
+      alt: "0개국어를 하는 느낌이죠.",
     },
     3: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02890e34f90fea4e88c2a_3-p-500.png",
-      alt: "네덜란드 국적을 갖고 싶습니다",
+      alt: "네덜란드 국적을 갖고 싶습니다.",
     },
     4: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028af49e78a2a884005b5_4-p-500.png",
-      alt: "지리적 고향에 대한 큰 의미를 갖고 있지 않습니다",
+      alt: "지리적 고향에 대한 큰 의미를 갖고 있지 않습니다.",
     },
     5: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028c1194ffa045ab61627_5-p-500.png",
-      alt: "구사할 수 있는 언어는 2.5개입니다",
+      alt: "",
     },
     6: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028d26ee14981f3c7707d_6-p-500.png",
-      alt: "왜 여기서 머물러야 하는지 구구절절한 설명과 여권, 거주증을 보여줘야 해요",
+      alt: "왜 여기서 머물러야 하는지 구구절절한 설명과 여권, 거주증을 보여줘야 해요.",
     },
     7: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028da110ab553be909204_7-p-500.png",
-      alt: "어디서든 이방인",
+      alt: "어디서든 이방인입니다.",
     },
     8: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028edd7afbc38e653ab09_8-p-500.png",
-      alt: "스타벅스에 가면 여러 버전의 이름이 생깁니다",
+      alt: "스타벅스에 가면 여러 버전의 이름이 생깁니다.",
     },
     9: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c028fe00fc8a8b507b76d2_9-p-500.png",
-      alt: "독일을 선택했다기 보다 집에서 최대한 멀어지고 싶었다",
+      alt: "독일을 선택했다기 보다 집에서 최대한 멀어지고 싶었다.",
     },
     10: {
       link: "https://uploads-ssl.webflow.com/61281e78cb914a06ecd1031c/61c02a35b5e590372de5152f_10-p-500.png",
-      alt: "누가 나를 어떻게 부르든 찰떡같이 알아듣게 됐습니다",
+      alt: "",
     },
   },
 };
